68ba6996d85a6d9ffddd71c143db3baf146c5a65
zverok <zverok.offline@gmail.com>
2015-10-06 18:00:18 +0300

    Some Request prototype

--- lib/mediawiktory.rb
+++ lib/mediawiktory.rb
@@ -14,4 +14,35 @@ module MediaWiktory
       @faraday.get('', params)
     end
   end
+
+  class Request
+    attr_reader :action, :params
+    
+    def initialize(action, params = {})
+      @action, @params = action, params
+    end
+
+    def query_hash
+      flatten(params).merge(action: action)
+    end
+
+    private
+
+    def flatten(hash, prefix: '')
+      res = {}
+      hash.each do |key, value|
+        case value
+        when Hash
+          res[key] = value.keys.join('|')
+          p value
+          res.merge!(value.map{|k,v| flatten(v)}.inject(:merge)) # TODO: flatten(v, prefix_from(k))
+        when Array
+          res[key] = value.join('|')
+        else
+          res[key] = value.to_s
+        end
+      end
+      res
+    end
+  end
 end
--- spec/mediawiktory/request_spec.rb
+++ spec/mediawiktory/request_spec.rb
@@ -1,9 +1,32 @@
 module MediaWiktory
   describe Request do
-    context 'validation' do
+    context 'construction' do
+      context 'action' do
+        subject{Request.new(:query)}
+        its(:action){should == :query}
+        its(:query_hash){should == {action: :query}}
+      end
+
+      context 'simple params' do
+        subject{Request.new(:query, prop: :categories)}
+        its(:query_hash){should == {action: :query, prop: 'categories'}}
+      end
+
+      context 'nested params' do
+        subject{Request.new(:query, prop: {categories: {clprop: [:sortkey,:timestamp]}})}
+        its(:query_hash){should == {action: :query, prop: 'categories', clprop: 'sortkey|timestamp'}}
+      end
     end
 
-    context 'construction' do
+    context 'validation' do
+      context 'unknown action' do
+      end
+
+      context 'unknown param' do
+      end
+
+      context 'missing param' do
+      end
     end
 
     context 'performing' do
