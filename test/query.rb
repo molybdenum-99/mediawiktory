7e1f25dd5fd87ba903aa238b359c2b2fd90217ed
zverok <zverok.offline@gmail.com>
2017-04-27 00:54:13 +0300

    Experiments

--- test/query.rb
+++ test/query.rb
@@ -4,4 +4,8 @@ require 'mediawiktory'
 require 'pp'
 
 #puts MediaWiktory::Wikipedia::Api.new.query.titles('Argentina').perform
-pp JSON.parse( MediaWiktory::Wikipedia::Api.new.query.titles('Argentina').prop(:revisions).limit('max').format(:json).perform )
+#pp JSON.parse( MediaWiktory::Wikipedia::Api.new.query.titles('Argentina').prop(:revisions).limit('max').format(:json).perform )
+
+
+pp JSON.parse( MediaWiktory::Wikipedia::Api.new.query.generator(:geosearch).page('London').format(:json).perform )
+# pp JSON.parse( MediaWiktory::Wikipedia::Api.new('https://ru.wikipedia.org/w/api.php').query.generator(:geosearch).limit(100).page('').format(:json).perform )
